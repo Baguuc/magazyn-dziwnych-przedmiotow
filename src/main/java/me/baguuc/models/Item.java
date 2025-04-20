3484b07994b6c9c901998b1db20c57a8c01f392e
Jan Żukowski <janzukowski@proton.me>
2025-04-20 09:44:55 +0200

    update: added Item model

---
+++ src/main/java/me/baguuc/models/Item.java
@@ -0,0 +1,36 @@
+package me.baguuc.models;
+
+import com.google.gson.Gson;
+
+public class Item {
+    private String name;
+    private float weightKg;
+    private int weirdnessLevel;
+    private boolean isSensitive;
+
+    public Item(String name, float weightKg, int weirdnessLevel, boolean isSensitive) {
+        this.name = name;
+        this.weightKg = weightKg;
+        this.weirdnessLevel = weirdnessLevel;
+        this.isSensitive = isSensitive;
+    }
+
+    public String description() {
+        record Inline(
+            String name,
+            float weightKg,
+            int weirdnessLevel,
+            String isSensitive
+        ) {}
+
+        Inline toSerialize = new Inline(
+            name,
+            weightKg,
+            weirdnessLevel,
+            isSensitive ? "TAK" : "NIE"
+        );
+        Gson mapper = new Gson();
+
+        return mapper.toJson(toSerialize);
+    }
+}
