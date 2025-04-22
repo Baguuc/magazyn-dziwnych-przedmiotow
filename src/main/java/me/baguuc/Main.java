b1b48156fe1f886099a886d81a9003a6afc1c9b0
Jan Żukowski <janzukowski@proton.me>
2025-04-22 18:08:41 +0200

    update: ended migration into spring boot

--- src/main/java/me/baguuc/Main.java
+++ src/main/java/me/baguuc/Main.java
@@ -1,13 +1,14 @@
 package me.baguuc;
 
+import me.baguuc.models.StorageManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
+	public static final StorageManager STORAGE_MANAGER = new StorageManager();
 
 	public static void main(String[] args) {
 		SpringApplication.run(Main.class, args);
 	}
-
 }
